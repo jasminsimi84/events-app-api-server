3cb065f42e83f3b3d6fd3a89a62fe1e301c7b904
sabdulshukkoor <sabdulshukkoor@deloitte.com>
2022-10-06 23:04:36 +0000

    Added files

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'TEAM EIGHT';
+    team = 'TEAM EIGHT Is GREAT';
     version = '1.0.1';
 }
 module.exports = AppConfig;
