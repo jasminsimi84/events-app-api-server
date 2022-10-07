2202cdc466e6d45898b2ad0509e062f2dbc3746c
jasminsimi84 <jasmin.simi@gmail.com>
2022-10-07 04:22:53 +0000

    somme message

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'TEAM EIGHT Is GOOD';
+    team = 'TEAM EIGHT Is GOOD ONE';
     version = '1.0.1';
 }
 module.exports = AppConfig;
